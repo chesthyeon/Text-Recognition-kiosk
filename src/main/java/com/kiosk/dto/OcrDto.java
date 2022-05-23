05b1c5c696ba5499cfe5e61f125a1f654da3587f
김승기 <57786933+SeungkeyMouse@users.noreply.github.com>
2022-05-23 14:21:35 +0900

    OcrDto생성
    ocr

---
+++ src/main/java/com/kiosk/dto/OcrDto.java
@@ -0,0 +1,12 @@
+package com.kiosk.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OcrDto {
+    private String name;
+    private Integer price;
+    private Integer count;
+}
