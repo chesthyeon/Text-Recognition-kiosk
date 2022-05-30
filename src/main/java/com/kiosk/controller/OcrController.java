55e0bd46729c9652a305a97ba9cbca374aa013b4
김승기 <57786933+SeungkeyMouse@users.noreply.github.com>
2022-05-30 12:10:43 +0900

    Update OcrController.java
    1. http://localhost:80/ocr/camera 를 url창에 치면 camera.html(teamplate폴더에 있음)호출함.
    2. 촬영시 http://localhost:80/ocr/ 로 's3에올린이미지주소' 를 requestparam으로 전달

--- src/main/java/com/kiosk/controller/OcrController.java
+++ src/main/java/com/kiosk/controller/OcrController.java
@@ -4,17 +4,25 @@ import com.kiosk.service.OcrService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URISyntaxException;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/ocr")
 public class OcrController {
 
     private final OcrService ocrService;
-    @GetMapping("/ocr/{url}")
-    ResponseEntity<?> addPost(@PathVariable("url") String url) {
+    @GetMapping("/")
+    ResponseEntity<?> addPost(@RequestParam("url") String url) {
+        System.out.println(url);
         return ResponseEntity.ok(ocrService.getOcr(url));
     }
 
+
+    @RequestMapping("/camera")
+    public ModelAndView index () {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("camera");
+        return modelAndView;
+    }
 }
